d9c377eb10d4a6e43ee675f835b838d0d87656e3
Doyoung Kim <doyoung9.kim@samsung.com>
2026-08-27 17:57:52 +0900

    feat: UserController 구현

---
+++ src/main/java/com/stayworld/back/user/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.stayworld.back.user.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    String userId;
+    String password;
+}
---
+++ src/main/java/com/stayworld/back/user/dto/ModifyDto.java
@@ -0,0 +1,9 @@
+package com.stayworld.back.user.dto;
+
+import lombok.Data;
+
+@Data
+public class ModifyDto {
+    String name;
+    String phoneNumber;
+}
