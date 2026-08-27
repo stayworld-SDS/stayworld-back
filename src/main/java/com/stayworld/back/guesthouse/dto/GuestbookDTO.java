273f0cdb667260992af0057056be243d5120b0c4
Jisu Hong <jaylovegood@snu.ac.kr>
2026-08-28 08:57:50 +0900

    Add guesthouse controller skeleton
    Guesthouse controller:
      GET /guesthouses
      GET /guesthouses/{id}
      GET /guesthouses/{id}/guestbooks
      POST /guesthouses/{id}/guestbooks

---
+++ src/main/java/com/stayworld/back/guesthouse/dto/GuestbookDTO.java
@@ -0,0 +1,17 @@
+package com.stayworld.back.guesthouse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuestbookDTO {
+    public int id;
+    public int userId;
+    public int guesthouseId;
+    public String body;
+}
---
+++ src/main/java/com/stayworld/back/guesthouse/dto/GuesthouseDTO.java
@@ -0,0 +1,22 @@
+package com.stayworld.back.guesthouse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuesthouseDTO {
+    public int id;
+    public String name;
+    public int price;
+    public String phoneNumber;
+    public String address;
+    public int capacity;
+    public boolean parking;
+    public boolean wifi;
+    public boolean breakfast;
+    public String introduction;
+    public int visitorCount;
+}
