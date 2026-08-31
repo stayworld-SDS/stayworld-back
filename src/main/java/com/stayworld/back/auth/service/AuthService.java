3b39aa19dc5dcd47a41890f90c28c6bfdce9084a
Doyoung Kim <doyoung9.kim@samsung.com>
2026-08-31 17:04:52 +0900

    feat: add password encryption

--- src/main/java/com/stayworld/back/auth/service/AuthService.java
+++ src/main/java/com/stayworld/back/auth/service/AuthService.java
@@ -5,18 +5,20 @@ import com.stayworld.back.global.exception.UnauthorizedException;
 import com.stayworld.back.user.entity.User;
 import com.stayworld.back.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public long login(LoginDto dto) {
         User user = userRepository.findByEmail(dto.getEmail())
                 .orElseThrow(() -> new UnauthorizedException("이메일 또는 비밀번호가 일치하지 않습니다."));
 
-        if (!user.getPassword().equals(dto.getPassword())) {
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
             throw new UnauthorizedException("이메일 또는 비밀번호가 일치하지 않습니다.");
         }
 
