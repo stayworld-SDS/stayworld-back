dfd22dfb6df2694112bced2da294d49d45cc613b
Doyoung Kim <doyoung9.kim@samsung.com>
2026-08-31 17:26:44 +0900

    feat: add e-mail and phone number validation

--- src/main/java/com/stayworld/back/user/dto/CreateDto.java
+++ src/main/java/com/stayworld/back/user/dto/CreateDto.java
@@ -1,11 +1,16 @@
 package com.stayworld.back.user.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateDto {
     String password;
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     String email;
+    @Size(min = 1, max = 20)
     String nickname;
+    @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$")
     String phoneNumber;
 }
