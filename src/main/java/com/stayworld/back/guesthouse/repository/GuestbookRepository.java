6fbff6a6aaa409f93dd3a1993bdcd1a346d7781d
Jisu Hong <jaylovegood@snu.ac.kr>
2026-09-01 16:27:58 +0900

    Enforce constraints for posting guestbooks

--- src/main/java/com/stayworld/back/guesthouse/repository/GuestbookRepository.java
+++ src/main/java/com/stayworld/back/guesthouse/repository/GuestbookRepository.java
@@ -2,8 +2,14 @@ package com.stayworld.back.guesthouse.repository;
 
 import com.stayworld.back.guesthouse.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+
+    @EntityGraph(attributePaths = "user")
+    Page<Guestbook> findByGuesthouseId(long guesthouseId, Pageable pageable);
 }
