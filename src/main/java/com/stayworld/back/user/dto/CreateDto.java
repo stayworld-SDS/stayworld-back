f4d4f436045dfa9bcc6db0e6161f02da13f17c47
Doyoung Kim <doyoung9.kim@samsung.com>
2026-09-02 15:56:59 +0900

    fix: fix test failure after the merge

--- src/main/java/com/stayworld/back/user/dto/CreateDto.java
+++ src/main/java/com/stayworld/back/user/dto/CreateDto.java
@@ -2,17 +2,19 @@ package com.stayworld.back.user.dto;
 
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class CreateDto {
+
     String password;
+
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     String email;
+
     @Size(min = 1, max = 20)
     String nickname;
+
     @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$")
     String phoneNumber;
 }
