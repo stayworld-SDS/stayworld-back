26c948aaa802ee1161c44de42c0e02d35060c31e
Taegeon Kim <61930500+Taegon21@users.noreply.github.com>
2026-09-03 16:42:19 +0900

    feat: swagger 업데이트

--- src/main/java/com/stayworld/back/global/config/WebConfig.java
+++ src/main/java/com/stayworld/back/global/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://stayworld-front.vercel.app")
+                .allowedOrigins("http://localhost:3000",
+                        "http://localhost:5173",
+                        "https://stayworld-front.vercel.app",
+                        "http://192.168.50.88:3000"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
